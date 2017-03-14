3a40b2e398072541f3ce7e7f98b8c93609931dfa
Narendra Arief Nugraha <narendra.arief@ui.ac.id>
2017-03-15 04:15:49 +0700

    Guideline buat frontend

--- src/main/java/id/ac/univ/regismaba/controller/MahasiswaController.java
+++ src/main/java/id/ac/univ/regismaba/controller/MahasiswaController.java
@@ -6,9 +6,9 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 public class MahasiswaController {
 
-	@RequestMapping("/login/mahasiswa")
+	@RequestMapping("/")
 	public String loginMahasiswa()
 	{
-		return "login";
+		return "index";
 	}
 }
