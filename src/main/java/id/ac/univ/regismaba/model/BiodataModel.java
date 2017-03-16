7be8357da803bdcd432b32c52da933989ad86f23
anindhitadwi <anindhita22@gmail.com>
2017-03-16 13:53:51 +0700

    add JenjangModel and BiodataModel

---
+++ src/main/java/id/ac/univ/regismaba/model/BiodataModel.java
@@ -0,0 +1,25 @@
+package id.ac.univ.regismaba.model;
+
+import java.util.Date;
+
+import lombok.Data;
+
+@Data
+public class BiodataModel {
+	int biodata_id;	
+	int data_kesehatan_id;	
+	String nomor_ijazah;
+	String nomor_asuransi;
+	int jalan_id;
+	Date tanggal_lahir;
+	String jenis_kelamin;
+	String nomor_telepon;
+	String kewarganegaraan;
+	String nomor_ktp;
+	String sidik_jari;
+	String scan_ktp;
+	String scan_kk;
+	String scan_surat_pernyataan_mahasiswa;
+	String status_verifikasi;
+	String flag_aktif;
+}
---
+++ src/main/java/id/ac/univ/regismaba/model/JenjangModel.java
@@ -0,0 +1,9 @@
+package id.ac.univ.regismaba.model;
+
+import lombok.Data;
+
+@Data
+public class JenjangModel {
+	int jenjang_id;
+	String nama_jenjang;
+}
