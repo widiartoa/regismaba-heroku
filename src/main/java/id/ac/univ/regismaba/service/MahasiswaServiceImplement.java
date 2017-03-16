829db56b87944ab1be87348559f311abdd051cf4
widiartoa <Artyos@Artyos-G501VW>
2017-03-16 21:44:37 +0700

    Update model and service

---
+++ src/main/java/id/ac/univ/regismaba/service/MahasiswaServiceImplement.java
@@ -0,0 +1,10 @@
+package id.ac.univ.regismaba.service;
+
+import org.springframework.stereotype.Service;
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+@Service
+public class MahasiswaServiceImplement implements MahasiswaService{
+
+}
