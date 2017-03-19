bf538f1735aeddd324170940bc30f1b6bc682753
sumarliyanti <sumarliyanti@github.com>
2017-03-19 14:25:27 +0700

    biodata service

--- src/main/java/id/ac/univ/regismaba/service/BiodataService.java
+++ src/main/java/id/ac/univ/regismaba/service/BiodataService.java
@@ -4,7 +4,5 @@ import id.ac.univ.regismaba.model.IjazahModel;
 
 public interface BiodataService
 {
-    IjazahModel selectIjazah(String nomor_ijazah);
     
-    void addIjazah(String nomor_ijazah);
 }
