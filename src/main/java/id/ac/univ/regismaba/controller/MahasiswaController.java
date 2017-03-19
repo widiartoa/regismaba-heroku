329858eac5097c762b73c2f0d1e427827777c938
Narendra Arief Nugraha <narendra.arief@ui.ac.id>
2017-03-20 00:23:51 +0700

    Bismillah persiapan merge

--- src/main/java/id/ac/univ/regismaba/controller/MahasiswaController.java
+++ src/main/java/id/ac/univ/regismaba/controller/MahasiswaController.java
@@ -1,19 +1,43 @@
 package id.ac.univ.regismaba.controller;
 
+import java.nio.file.Path;
+import java.util.List;
+import java.util.Random;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.io.Resource;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.servlet.mvc.method.annotation.MvcUriComponentsBuilder;
 
 import id.ac.univ.regismaba.model.MahasiswaModel;
+import id.ac.univ.regismaba.model.PengajuanSkemaBiayaModel;
+import id.ac.univ.regismaba.model.SkemaBiayaModel;
 import id.ac.univ.regismaba.service.MahasiswaService;
+import id.ac.univ.regismaba.service.PengajuanSkemaBiayaService;
+import id.ac.univ.regismaba.service.SkemaBiayaService;
+import id.ac.univ.regismaba.storage.StorageFileNotFoundException;
+import id.ac.univ.regismaba.storage.StorageService;
 
 @Controller
 public class MahasiswaController {
-
+	private final StorageService storageService;
+	
+	@Autowired
+	public MahasiswaController(StorageService storageService){
+		this.storageService = storageService;
+	}
+	
     @Autowired
     MahasiswaService mahasiswaDAO;
 	
@@ -51,11 +75,13 @@ public class MahasiswaController {
 		return "calon_mahasiswa-survey_kesehatan";
 	}
 	
-	/*@RequestMapping("/calon-mahasiswa/survey-kesehatan/submit")
+	@RequestMapping("/calon-mahasiswa/survey-kesehatan/submit")
 	public String submitSurveyKesehatan(Model model, 
-		@RequestParam(value = "form_survey_kesehatan", required = false) String username
+		@RequestParam(value = "form_survey_kesehatan", required = false) MultipartFile form_survey_kesehatan
 	)	{
+		MahasiswaModel mahasiswa = mahasiswaDAO.selectMahasiswa("1234567890");
+		storageService.store(form_survey_kesehatan, mahasiswa.npm);
 		
 		return "calon_mahasiswa-survey_kesehatan";
-	}*/
+	}
 }
