5eb7ae64444d6e4783400148d558afc14e063b66
sumarliyanti <sumarliyanti@github.com>
2017-03-19 14:19:52 +0700

    ijazah model

--- src/main/java/id/ac/univ/regismaba/model/IjazahModel.java
+++ src/main/java/id/ac/univ/regismaba/model/IjazahModel.java
@@ -1,8 +1,12 @@
 package id.ac.univ.regismaba.model;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class IjazahModel
 {
     String nomor_ijazah;
