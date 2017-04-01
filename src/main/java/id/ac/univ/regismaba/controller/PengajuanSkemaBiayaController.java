2816f20304d84228b629edc9971a7166ece6c4a5
widiartoa <widiartoaww@gmail.com>
2017-04-01 16:07:43 +0700

    Validation for storage completed, hopefully

--- src/main/java/id/ac/univ/regismaba/controller/PengajuanSkemaBiayaController.java
+++ src/main/java/id/ac/univ/regismaba/controller/PengajuanSkemaBiayaController.java
@@ -37,6 +37,8 @@ public class PengajuanSkemaBiayaController {
 
 	private final StorageService storageService;
 	private static final List<String> contentTypes = Arrays.asList("image/png", "image/jpeg");
+	private boolean fileError = false;
+	private PengajuanSkemaBiayaModel skema = new PengajuanSkemaBiayaModel();
 	
 	@Autowired
 	public PengajuanSkemaBiayaController(StorageService storageService){
@@ -112,7 +114,6 @@ public class PengajuanSkemaBiayaController {
 												@RequestParam("nilai_tagihan_telepon") String nilai_tagihan_telepon)
 	{
 		MahasiswaModel mahasiswa = mahasiswaService.selectMahasiswa("1234567890");
-		PengajuanSkemaBiayaModel skema = new PengajuanSkemaBiayaModel();
 		
 		System.out.println("nih golongan " + golongan_id);
 		skema.setGolongan_id(golongan_id);
@@ -159,151 +160,182 @@ public class PengajuanSkemaBiayaController {
 			skema.setNilai_tagihan_telepon(0);
 		}
 		
-		Random rand = new Random();
-		int num = rand.nextInt(1000000) + 1;
+		storeFile(surat_keterangan_rtrw, 1);
+		storeFile(foto_rumah, 2);
+		storeFile(slip_gaji_pribadi, 3);
+		storeFile(slip_gaji_wali1, 4);
+		storeFile(slip_gaji_wali2, 5);
+		storeFile(tagihan_air, 6);
+		storeFile(tagihan_listrik, 7);
+		storeFile(tagihan_telepon, 8);
 		
-		if(surat_keterangan_rtrw.isEmpty() == false){
-			storageService.store(surat_keterangan_rtrw, num + "-1");
-			
-			//SURAT KETERANGAN RT RW UPLOAD //
-	        String pathDB1 = storageService.load(surat_keterangan_rtrw.getOriginalFilename()).toString();
-	        
-	        Path data1 = storageService.load(surat_keterangan_rtrw.getOriginalFilename());
-	        String pdb1 = MvcUriComponentsBuilder
-	                .fromMethodName(PengajuanSkemaBiayaController.class, "serveFile", num + "-1-" + data1.getFileName().toString())
-	                .build().toString();
-	        
-	        skema.setSurat_keterangan_rtrw(pdb1);
-		}
-		else{
-			skema.setSurat_keterangan_rtrw("no_data");
-		}
 		
-		if(foto_rumah.isEmpty() == false){
-			storageService.store(foto_rumah, num + "-2");
-			
-	        //FOTO RUMAH//
-	        String pathDB2 = storageService.load(foto_rumah.getOriginalFilename()).toString();
-	        
-	        Path data2 = storageService.load(foto_rumah.getOriginalFilename());
-	        String pdb2 = MvcUriComponentsBuilder
-	                .fromMethodName(PengajuanSkemaBiayaController.class, "serveFile", num + "-2-" + data2.getFileName().toString())
-	                .build().toString();
-	        
-	        skema.setFoto_rumah(pdb2);
-		}
-		else {
-			skema.setFoto_rumah("no_data");
-		}
-		
-		if(slip_gaji_pribadi.isEmpty() == false){
-			storageService.store(slip_gaji_pribadi, num + "-3");
-			
-	        //SLIP GAJI PRIBADI//
-	        String pathDB3 = storageService.load(slip_gaji_pribadi.getOriginalFilename()).toString();
-	        
-	        Path data3 = storageService.load(slip_gaji_pribadi.getOriginalFilename());
-	        String pdb3 = MvcUriComponentsBuilder
-	                .fromMethodName(PengajuanSkemaBiayaController.class, "serveFile", num + "-3-" + data3.getFileName().toString())
-	                .build().toString();
-	        
-	        skema.setSlip_gaji_pribadi(pdb3);
-		}
-		else{
-			skema.setSlip_gaji_pribadi("no_data");
-		}
-		
-		if(slip_gaji_wali1.isEmpty() == false)
-		{
-			storageService.store(slip_gaji_wali1, num + "-4");
-			
-	        //SLIP GAJI WALI 1//
-	        String pathDB4 = storageService.load(slip_gaji_wali1.getOriginalFilename()).toString();
-	        
-	        Path data4 = storageService.load(slip_gaji_wali1.getOriginalFilename());
-	        String pdb4 = MvcUriComponentsBuilder
-	                .fromMethodName(PengajuanSkemaBiayaController.class, "serveFile", num + "-4-" + data4.getFileName().toString())
-	                .build().toString();
-	        
-	        skema.setSlip_gaji_wali1(pdb4);
-		}
-		else{
-			skema.setSlip_gaji_wali1("no_data");
-		}
-		
-		if(slip_gaji_wali2.isEmpty() == false)
-		{
-			storageService.store(slip_gaji_wali2, num + "-5");
-			
-	        //SLIP GAJI WALI 2//
-	        String pathDB5 = storageService.load(slip_gaji_wali2.getOriginalFilename()).toString();
-	        
-	        Path data5 = storageService.load(slip_gaji_wali2.getOriginalFilename());
-	        String pdb5 = MvcUriComponentsBuilder
-	                .fromMethodName(PengajuanSkemaBiayaController.class, "serveFile", num + "-5-" + data5.getFileName().toString())
-	                .build().toString();
-	        
-	        skema.setSlip_gaji_wali2(pdb5);
-		}
-		else{
-			skema.setSlip_gaji_wali2("no_data");
-		}
-			
-		
-		if(tagihan_air.isEmpty() == false)
-		{
-			storageService.store(tagihan_air, num + "-6");
-			
-	        //TAGIHAN AIR//
-	        String pathDB6 = storageService.load(tagihan_air.getOriginalFilename()).toString();
-	        
-	        Path data6 = storageService.load(tagihan_air.getOriginalFilename());
-	        String pdb6 = MvcUriComponentsBuilder
-	                .fromMethodName(PengajuanSkemaBiayaController.class, "serveFile", num + "-6-" + data6.getFileName().toString())
-	                .build().toString();
-	        
-	        skema.setTagihan_air(pdb6);
-		}
-		else{
-			skema.setTagihan_air("no_data");
-		}
-		
-		if(tagihan_listrik.isEmpty() == false)
-		{
-			storageService.store(tagihan_listrik, num + "-7");
-			
-	        //TAGIHAN LISTRIK//
-	        String pathDB7 = storageService.load(tagihan_listrik.getOriginalFilename()).toString();
-	        
-	        Path data7 = storageService.load(tagihan_listrik.getOriginalFilename());
-	        String pdb7 = MvcUriComponentsBuilder
-	                .fromMethodName(PengajuanSkemaBiayaController.class, "serveFile", num + "-7-" + data7.getFileName().toString())
-	                .build().toString();
-	        
-	        skema.setTagihan_listrik(pdb7);
-		}
-		else{
-			skema.setTagihan_listrik("no_data");
-		}
-		
-		if(tagihan_telepon.isEmpty() == false)
-		{
-			storageService.store(tagihan_telepon, num + "-8");
-			
-	        //TAGIHAN TELEPON//
-	        String pathDB8 = storageService.load(tagihan_telepon.getOriginalFilename()).toString();
-	        
-	        Path data8 = storageService.load(tagihan_telepon.getOriginalFilename());
-	        String pdb8 = MvcUriComponentsBuilder
-	                .fromMethodName(PengajuanSkemaBiayaController.class, "serveFile", num + "-8-" + data8.getFileName().toString())
-	                .build().toString();
-	        
-	        skema.setTagihan_telepon(pdb8);
-		}
-		else{
-			skema.setTagihan_telepon("no_data");
-		}
+//		Random rand = new Random();
+//		int num = rand.nextInt(1000000) + 1;
+//		
+//		if(surat_keterangan_rtrw.isEmpty() == false){	
+//		    String fileContentType = surat_keterangan_rtrw.getContentType();
+//		    if(contentTypes.contains(fileContentType)) {
+//				storageService.store(surat_keterangan_rtrw, num + "-1");
+//				
+//				//SURAT KETERANGAN RT RW UPLOAD //
+//		        String pathDB1 = storageService.load(surat_keterangan_rtrw.getOriginalFilename()).toString();
+//		        
+//		        Path data1 = storageService.load(surat_keterangan_rtrw.getOriginalFilename());
+//		        String pdb1 = MvcUriComponentsBuilder
+//		                .fromMethodName(PengajuanSkemaBiayaController.class, "serveFile", num + "-1-" + data1.getFileName().toString())
+//		                .build().toString();
+//		        
+//		        skema.setSurat_keterangan_rtrw(pdb1);
+//		    } else {
+//		    	return "calon_mahasiswa-salah_file_pengajuan";
+//		    }
+//		}
+//		else{
+//			if(psbs.selectPSBMFromUsername(mahasiswa.getUsername()) != null){
+//				skema.setSurat_keterangan_rtrw(psbs.selectPSBMFromUsername(mahasiswa.getUsername()).getSurat_keterangan_rtrw());
+//			}
+//			else{
+//				return "calon_mahasiswa-salah_file_pengajuan";
+//			}
+//		}
+//		
+//		if(foto_rumah.isEmpty() == false){
+//		    String fileContentType = foto_rumah.getContentType();
+//		    if(contentTypes.contains(fileContentType)) {
+//				storageService.store(foto_rumah, num + "-2");
+//				
+//		        //FOTO RUMAH//
+//		        String pathDB2 = storageService.load(foto_rumah.getOriginalFilename()).toString();
+//		        
+//		        Path data2 = storageService.load(foto_rumah.getOriginalFilename());
+//		        String pdb2 = MvcUriComponentsBuilder
+//		                .fromMethodName(PengajuanSkemaBiayaController.class, "serveFile", num + "-2-" + data2.getFileName().toString())
+//		                .build().toString();
+//		        
+//		        skema.setFoto_rumah(pdb2);
+//		    } else {
+//		    	return "calon_mahasiswa-salah_file_pengajuan";
+//		    }
+//		}
+//		else {
+//			skema.setFoto_rumah("no_data");
+//		}
+//		
+//		if(slip_gaji_pribadi.isEmpty() == false){
+//			storageService.store(slip_gaji_pribadi, num + "-3");
+//			
+//	        //SLIP GAJI PRIBADI//
+//	        String pathDB3 = storageService.load(slip_gaji_pribadi.getOriginalFilename()).toString();
+//	        
+//	        Path data3 = storageService.load(slip_gaji_pribadi.getOriginalFilename());
+//	        String pdb3 = MvcUriComponentsBuilder
+//	                .fromMethodName(PengajuanSkemaBiayaController.class, "serveFile", num + "-3-" + data3.getFileName().toString())
+//	                .build().toString();
+//	        
+//	        skema.setSlip_gaji_pribadi(pdb3);
+//		}
+//		else{
+//			skema.setSlip_gaji_pribadi("no_data");
+//		}
+//		
+//		if(slip_gaji_wali1.isEmpty() == false)
+//		{
+//			storageService.store(slip_gaji_wali1, num + "-4");
+//			
+//	        //SLIP GAJI WALI 1//
+//	        String pathDB4 = storageService.load(slip_gaji_wali1.getOriginalFilename()).toString();
+//	        
+//	        Path data4 = storageService.load(slip_gaji_wali1.getOriginalFilename());
+//	        String pdb4 = MvcUriComponentsBuilder
+//	                .fromMethodName(PengajuanSkemaBiayaController.class, "serveFile", num + "-4-" + data4.getFileName().toString())
+//	                .build().toString();
+//	        
+//	        skema.setSlip_gaji_wali1(pdb4);
+//		}
+//		else{
+//			skema.setSlip_gaji_wali1("no_data");
+//		}
+//		
+//		if(slip_gaji_wali2.isEmpty() == false)
+//		{
+//			storageService.store(slip_gaji_wali2, num + "-5");
+//			
+//	        //SLIP GAJI WALI 2//
+//	        String pathDB5 = storageService.load(slip_gaji_wali2.getOriginalFilename()).toString();
+//	        
+//	        Path data5 = storageService.load(slip_gaji_wali2.getOriginalFilename());
+//	        String pdb5 = MvcUriComponentsBuilder
+//	                .fromMethodName(PengajuanSkemaBiayaController.class, "serveFile", num + "-5-" + data5.getFileName().toString())
+//	                .build().toString();
+//	        
+//	        skema.setSlip_gaji_wali2(pdb5);
+//		}
+//		else{
+//			skema.setSlip_gaji_wali2("no_data");
+//		}
+//			
+//		
+//		if(tagihan_air.isEmpty() == false)
+//		{
+//			storageService.store(tagihan_air, num + "-6");
+//			
+//	        //TAGIHAN AIR//
+//	        String pathDB6 = storageService.load(tagihan_air.getOriginalFilename()).toString();
+//	        
+//	        Path data6 = storageService.load(tagihan_air.getOriginalFilename());
+//	        String pdb6 = MvcUriComponentsBuilder
+//	                .fromMethodName(PengajuanSkemaBiayaController.class, "serveFile", num + "-6-" + data6.getFileName().toString())
+//	                .build().toString();
+//	        
+//	        skema.setTagihan_air(pdb6);
+//		}
+//		else{
+//			skema.setTagihan_air("no_data");
+//		}
+//		
+//		if(tagihan_listrik.isEmpty() == false)
+//		{
+//			storageService.store(tagihan_listrik, num + "-7");
+//			
+//	        //TAGIHAN LISTRIK//
+//	        String pathDB7 = storageService.load(tagihan_listrik.getOriginalFilename()).toString();
+//	        
+//	        Path data7 = storageService.load(tagihan_listrik.getOriginalFilename());
+//	        String pdb7 = MvcUriComponentsBuilder
+//	                .fromMethodName(PengajuanSkemaBiayaController.class, "serveFile", num + "-7-" + data7.getFileName().toString())
+//	                .build().toString();
+//	        
+//	        skema.setTagihan_listrik(pdb7);
+//		}
+//		else{
+//			skema.setTagihan_listrik("no_data");
+//		}
+//		
+//		if(tagihan_telepon.isEmpty() == false)
+//		{
+//			storageService.store(tagihan_telepon, num + "-8");
+//			
+//	        //TAGIHAN TELEPON//
+//	        String pathDB8 = storageService.load(tagihan_telepon.getOriginalFilename()).toString();
+//	        
+//	        Path data8 = storageService.load(tagihan_telepon.getOriginalFilename());
+//	        String pdb8 = MvcUriComponentsBuilder
+//	                .fromMethodName(PengajuanSkemaBiayaController.class, "serveFile", num + "-8-" + data8.getFileName().toString())
+//	                .build().toString();
+//	        
+//	        skema.setTagihan_telepon(pdb8);
+//		}
+//		else{
+//			skema.setTagihan_telepon("no_data");
+//		}
         
+		
+		if(fileError == true)
+		{
+			return "calon_mahasiswa-salah_file_pengajuan";
+		}
+		
 		//INSERT OR UPDATE//
 		if(psbs.selectPSBMFromUsername(mahasiswa.getUsername()) == null)
 		{
@@ -340,4 +372,73 @@ public class PengajuanSkemaBiayaController {
     public ResponseEntity handleStorageFileNotFound(StorageFileNotFoundException exc) {
         return ResponseEntity.notFound().build();
     }
+    
+    public void storeFile(MultipartFile file, int type)
+    {
+    	MahasiswaModel mahasiswa = mahasiswaService.selectMahasiswa("1234567890");
+    	Random rand = new Random();
+		int num = rand.nextInt(1000000) + 1;
+		
+    	if(file.isEmpty() == false){	
+    		String fileContentType = file.getContentType();
+    		
+    		if(contentTypes.contains(fileContentType)) {
+    			storageService.store(file, num + "-1");
+    			
+    			String path = storageService.load(file.getOriginalFilename()).toString();
+    			
+    			Path data = storageService.load(file.getOriginalFilename());
+    			String dbURL = MvcUriComponentsBuilder
+    					.fromMethodName(PengajuanSkemaBiayaController.class, "serveFile", num + "-" + type + "-" + data.getFileName().toString())
+    					.build().toString();
+    			
+    			if(type == 1) {skema.setSurat_keterangan_rtrw(dbURL);}
+    			if(type == 2) {skema.setFoto_rumah(dbURL);}
+    			if(type == 3) {skema.setSlip_gaji_pribadi(dbURL);}
+    			if(type == 4) {skema.setSlip_gaji_wali1(dbURL);}
+    			if(type == 5) {skema.setSlip_gaji_wali2(dbURL);}
+    			if(type == 6) {skema.setTagihan_air(dbURL);}
+    			if(type == 7) {skema.setTagihan_listrik(dbURL);}
+    			if(type == 8) {skema.setTagihan_telepon(dbURL);}
+    			
+    		} else {
+    			//return "calon_mahasiswa-salah_file_pengajuan";
+    			//return fileError;
+    			fileError = true;
+    		}
+    	}
+    	else{
+    		//dont update, pengajuan exist
+    		if(psbs.selectPSBMFromUsername(mahasiswa.getUsername()) != null){
+    			PengajuanSkemaBiayaModel tmp = psbs.selectPSBMFromUsername(mahasiswa.getUsername());
+    			skema.setSurat_keterangan_rtrw(psbs.selectPSBMFromUsername(mahasiswa.getUsername()).getSurat_keterangan_rtrw());
+    			if(type == 1) {skema.setSurat_keterangan_rtrw(tmp.getSurat_keterangan_rtrw());}
+    			if(type == 2) {skema.setFoto_rumah(tmp.getFoto_rumah());}
+    			if(type == 3) {skema.setSlip_gaji_pribadi(tmp.getSlip_gaji_pribadi());}
+    			if(type == 4) {skema.setSlip_gaji_wali1(tmp.getSlip_gaji_wali1());}
+    			if(type == 5) {skema.setSlip_gaji_wali2(tmp.getSlip_gaji_wali2());}
+    			if(type == 6) {skema.setTagihan_air(tmp.getTagihan_air());}
+    			if(type == 7) {skema.setTagihan_listrik(tmp.getTagihan_listrik());}
+    			if(type == 8) {skema.setTagihan_telepon(tmp.getTagihan_telepon());}
+    		}
+    		//insert failed
+    		else{
+    			//return "calon_mahasiswa-salah_file_pengajuan";
+    			if( type == 1 ||
+    				type == 2 ||
+    				type == 6 ||
+    				type == 7 ||
+    				type == 8)
+    			{
+        			fileError = true;
+    			}
+    			else
+    			{
+    				if(type == 3) {skema.setSlip_gaji_pribadi("no_data");}
+        			if(type == 4) {skema.setSlip_gaji_wali1("no_data");}
+        			if(type == 5) {skema.setSlip_gaji_wali2("no_data");}
+    			}
+    		}
+    	}
+    }		
 }
