a941d38ae8e40e2130883ac012a0d47be099cf34
anindhitadwi <anindhita22@gmail.com>
2017-04-17 21:32:34 +0700

    cicil sambil nunggu database

--- src/main/java/id/ac/univ/regismaba/model/MahasiswaModel.java
+++ src/main/java/id/ac/univ/regismaba/model/MahasiswaModel.java
@@ -9,11 +9,15 @@ public class MahasiswaModel {
 	String username;
 	String npm;
 	String uid_sso;
-	int jenjang_id;
+	int program_studi_id;
 	String no_seleksi;
 	int jadwal_registrasi_id;
 	int jadwal_tes_kesehatan_id;
 	int jadwal_ept_id;
 	BiodataModel biodata;
+	ProgramStudiModel program_studi;
+	String program;
+	String fakultas;
+	String jenjang;
 	
 }
