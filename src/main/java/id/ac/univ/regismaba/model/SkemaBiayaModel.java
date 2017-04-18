f9f03568c2df2dd5acf7caff260adc998a20e014
widiartoa <widiartoaww@gmail.com>
2017-04-18 14:09:15 +0700

    Split Skema's list to 3 different payment groups

--- src/main/java/id/ac/univ/regismaba/model/SkemaBiayaModel.java
+++ src/main/java/id/ac/univ/regismaba/model/SkemaBiayaModel.java
@@ -6,6 +6,10 @@ import lombok.Data;
 public class SkemaBiayaModel
 {
     int golongan_id;
-    int jumlah_biaya;
-    int uang_pangkal;
+    int jumlah_biaya_ipa;
+    int uang_pangkal_ipa;
+    int jumlah_biaya_ips;
+    int uang_pangkal_ips;
+    int jumlah_biaya_ik;
+    int uang_pangkal_ik;
 }
