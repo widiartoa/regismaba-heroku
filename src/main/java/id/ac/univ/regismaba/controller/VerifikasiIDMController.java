0ee390a012bebc032ea4b7e0ee5021e154f28d8e
sumarliyanti <sumarliyanti15@gmail.com>
2017-04-19 23:58:16 +0700

    updated controller untuk verifikasiIDM

--- src/main/java/id/ac/univ/regismaba/controller/VerifikasiIDMController.java
+++ src/main/java/id/ac/univ/regismaba/controller/VerifikasiIDMController.java
@@ -3,23 +3,27 @@ package id.ac.univ.regismaba.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import id.ac.univ.regismaba.model.BiodataModel;
 import id.ac.univ.regismaba.model.MahasiswaModel;
 import id.ac.univ.regismaba.service.BiodataService;
 import id.ac.univ.regismaba.service.MahasiswaService;
+import id.ac.univ.regismaba.service.VerifikasiIDMService;
 
 @Controller
 public class VerifikasiIDMController
 {   
-    private 
+    private BiodataModel biodata = new BiodataModel();
     @Autowired
     MahasiswaService mahasiswaService;
     
     @Autowired
     BiodataService biodataService;
+    
+    @Autowired
+    VerifikasiIDMService verifikasiIdmService;
     
     @RequestMapping("/staf_verifikasi/detailIDM/")
     public String detailIDM(Model model) {
@@ -28,6 +32,7 @@ public class VerifikasiIDMController
        
         if(mahasiswa != null) {
 // if(biodata.get status verif == not yet) masuk sini
+//            String status_verifikasi = verifikasiIdmService
             model.addAttribute ("mahasiswa", mahasiswa);
 //            model.addAttribute ("biodata", biodata);
             return "staf_verifikasi-detail_idm_mhs";
@@ -40,12 +45,12 @@ public class VerifikasiIDMController
     
     @RequestMapping("/staf_verifikasi/detailIDM/verified")
     public String VerifiedDetailIDM(Model model) {
+//        status = "Verified";
         MahasiswaModel mahasiswa = mahasiswaService.selectMahasiswaByUsername ("benathavia.saladdin");
-       
+        verifikasiIdmService.updateStatus (mahasiswa.getUsername ());
         if(mahasiswa != null) {
             model.addAttribute ("mahasiswa", mahasiswa);
-//            model.addAttribute ("biodata", biodata);
-            return "staf_verifikasi-detail_idm_mhs";
+            return "staf_verifikasi-detail_idm_mhs_verified";
         } else {
             return "not-registered";
         }
