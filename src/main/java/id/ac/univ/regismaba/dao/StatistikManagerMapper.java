5347219ce73e8e6cb9ea6a0f2c6d8312173dfcac
widiartoa <widiartoaww@gmail.com>
2017-05-02 15:01:57 +0700

    Front end created. Still have logic error in output

--- src/main/java/id/ac/univ/regismaba/dao/StatistikManagerMapper.java
+++ src/main/java/id/ac/univ/regismaba/dao/StatistikManagerMapper.java
@@ -16,22 +16,22 @@ import id.ac.univ.regismaba.model.StatistikManagerModel;
 public interface StatistikManagerMapper {
 
 	@Select("select f.nama_fakultas as jenis_rekap, count(*) as jumlah from fakultas f, program_studi p, "
-			+ "mahasiswa m, where f.fakultas_id=p.fakultas_id and p.program_studi_id=m.program_studi_id and "
+			+ "mahasiswa m where f.fakultas_id=p.fakultas_id and p.program_studi_id=m.program_studi_id and "
 			+ "f.fakultas_id=#{fakultas_id}")
 	StatistikManagerModel selectPemilihFakultas(@Param("fakultas_id") int fakultas_id);
 	
 	@Select("select p.nama_program_studi as jenis_rekap, count(*) as jumlah from program_studi p, mahasiswa m "
-			+ "where p.program_studi_id=m.program_studi_id and p.program_studi_id={program_studi_id}")
+			+ "where p.program_studi_id=m.program_studi_id and p.program_studi_id=#{program_studi_id}")
 	StatistikManagerModel selectPemilihProdi(@Param("program_studi_id") int program_studi_id);
 	
 	@Select("select j.nama_jenjang as jenis_rekap, count(*) as jumlah from jenjang j, program_studi p, "
 			+ "mahasiswa m where j.jenjang_id=p.jenjang_id and p.program_studi_id=m.program_studi_id and "
-			+ "j.jenjang_id={jenjang_id}")
+			+ "j.jenjang_id=#{jenjang_id}")
 	StatistikManagerModel selectJenjang(@Param("jenjang_id") int jenjang_id);
 	
 	@Select("select p.nama_program as jenis_rekap, count(*) as jumlah from program p, program_studi s, "
 			+ "mahasiswa m where p.program_id=s.program_id and s.program_studi_id=m.program_studi_id and "
-			+ "p.program_id={program_id}")
+			+ "p.program_id=#{program_id}")
 	StatistikManagerModel selectProgram(@Param("program_id") int program_id);
 	
 	@Select("select distinct f.fakultas_id, f.nama_fakultas from fakultas f, program_studi p, mahasiswa m where "
