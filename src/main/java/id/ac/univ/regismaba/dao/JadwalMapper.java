6f3d17df477bb39ea762cb7931967fc02f7728f2
anindhitadwi <anindhita22@gmail.com>
2017-05-06 17:56:22 +0700

    Menghapus Jadwal Done

--- src/main/java/id/ac/univ/regismaba/dao/JadwalMapper.java
+++ src/main/java/id/ac/univ/regismaba/dao/JadwalMapper.java
@@ -6,6 +6,7 @@ import org.apache.ibatis.annotations.Insert;
 import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Param;
 import org.apache.ibatis.annotations.Select;
+import org.apache.ibatis.annotations.Update;
 
 import id.ac.univ.regismaba.model.JadwalEptModel;
 import id.ac.univ.regismaba.model.JadwalKesehatanModel;
@@ -23,13 +24,16 @@ public interface JadwalMapper {
 	@Select("select * from jadwal_registrasi where jadwal_registrasi_id = #{jadwal_registrasi_id}")
 	JadwalRegisModel selectJadwalRegis(@Param("jadwal_registrasi_id") int jadwal_registrasi_id);
 	
-	@Select("select timestamp_awal, timestamp_akhir, kapasitas, created_by, created_at, updated_by, updated_at from jadwal_registrasi WHERE status_aktif = 1 ORDER BY created_at DESC")
+	@Select("select jadwal_registrasi_id, timestamp_awal, timestamp_akhir, kapasitas, created_by, created_at, updated_by, updated_at from jadwal_registrasi WHERE status_aktif = 1 ORDER BY created_at DESC")
 	List<JadwalRegisModel> selectAllJadwalRegis();
 	
-	@Select("select timestamp_awal, timestamp_akhir, kapasitas, created_by, created_at, updated_by, updated_at from jadwal_registrasi WHERE tahun_ajaran_id = #{tahun_ajaran_id} and status_aktif = 1 ORDER BY created_at DESC")
+	@Select("select jadwal_registrasi_id, timestamp_awal, timestamp_akhir, kapasitas, created_by, created_at, updated_by, updated_at from jadwal_registrasi WHERE tahun_ajaran_id = #{tahun_ajaran_id} and status_aktif = 1 ORDER BY created_at DESC")
 	List<JadwalRegisModel> selectAllJadwalRegisbyTahunAjaran(@Param("tahun_ajaran_id") int tahun_ajaran_id);
 	
 	@Insert("insert into jadwal_registrasi (timestamp_awal, timestamp_akhir, kapasitas, created_by, created_at, updated_by, updated_at, tahun_ajaran_id, status_aktif) "
 			+ "values (#{timestamp_awal}, #{timestamp_akhir}, #{kapasitas}, #{created_by}, current_timestamp, #{updated_by}, current_timestamp, #{tahun_ajaran_id}, 1)")
 	void insertJadwalRegis(JadwalRegisModel jadwalRegis);
+	
+	@Update("update jadwal_registrasi set status_aktif = 0 where jadwal_registrasi_id = #{jadwal_registrasi_id}")
+    void deleteJadwalRegis (@Param("jadwal_registrasi_id") int jadwal_registrasi_id);
 }
