430d2b9593481208e32c5590bc717c2486cccd58
widiartoa <widiartoaww@gmail.com>
2017-05-06 17:23:05 +0700

    Tingkat Role adjusted for Staf Kesejahteraan's Rincian Skema

--- src/main/java/id/ac/univ/regismaba/dao/SkemaBiayaMapper.java
+++ src/main/java/id/ac/univ/regismaba/dao/SkemaBiayaMapper.java
@@ -4,19 +4,25 @@ import java.util.List;
 
 import org.apache.ibatis.annotations.Insert;
 import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.One;
 import org.apache.ibatis.annotations.Param;
+import org.apache.ibatis.annotations.Result;
+import org.apache.ibatis.annotations.Results;
 import org.apache.ibatis.annotations.Select;
 import org.apache.ibatis.annotations.Update;
 
 import id.ac.univ.regismaba.model.SkemaBiayaModel;
+import id.ac.univ.regismaba.model.TingkatRoleModel;
 
 @Mapper
 public interface SkemaBiayaMapper {
 
 	@Select("select * from skema_pembayaran where golongan_id = #{golongan_id}")
+	@Results(@Result(property="tingkat_role", column="tingkat_role_id", javaType=TingkatRoleModel.class, one=@One(select="selectRole")))
 	SkemaBiayaModel selectSBM(@Param("golongan_id") int golongan_id);
 	
 	@Select("select * from skema_pembayaran")
+	@Results(@Result(property="tingkat_role", column="tingkat_role_id", javaType=TingkatRoleModel.class, one=@One(select="selectRole")))
 	List<SkemaBiayaModel> selectAllSBM();
 	
 	@Insert("insert into skema_pembayaran (jumlah_biaya_ipa, uang_pangkal_ipa, jumlah_biaya_ips, uang_pangkal_ips, "
@@ -30,4 +36,6 @@ public interface SkemaBiayaMapper {
 			+ "where golongan_id=#{golongan_id}")
 	void updateSBM(SkemaBiayaModel sbm);
 	
+	@Select("select * from tingkat_role where tingkat_role_id=#{tingkat_role_id}")
+	TingkatRoleModel selectRole(@Param("tingkat_role_id") int tingkat_role_id);
 }
