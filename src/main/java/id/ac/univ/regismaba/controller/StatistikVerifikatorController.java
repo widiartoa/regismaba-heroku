c6b87a2ad5af003a1c22ae015f33578f85a31df3
sumarliyanti <sumarliyanti15@gmail.com>
2017-05-20 03:12:26 +0700

    controller for analytics verifikator

--- src/main/java/id/ac/univ/regismaba/controller/StatistikVerifikatorController.java
+++ src/main/java/id/ac/univ/regismaba/controller/StatistikVerifikatorController.java
@@ -8,10 +8,10 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-import id.ac.univ.regismaba.model.JalurModel;
-import id.ac.univ.regismaba.model.StatistikManagerModel;
-import id.ac.univ.regismaba.model.StatistikManagerSummaryModel;
+import id.ac.univ.regismaba.model.FakultasModel;
+import id.ac.univ.regismaba.model.StatistikVerifikatorSummaryModel;
 import id.ac.univ.regismaba.service.StatistikManagerService;
+import id.ac.univ.regismaba.service.StatistikVerifikatorService;
 
 @Controller
 public class StatistikVerifikatorController
@@ -19,30 +19,134 @@ public class StatistikVerifikatorController
     @Autowired
     StatistikVerifikatorService svs;
     
-    @RequestMapping("/manajer-pendidikan/statistik-verifikator/jalur")
-    public String statistikVerifJalur (Model model)
+    @Autowired
+    StatistikManagerService sms;
+    
+    @RequestMapping("/manager-pendidikan/statistik-verifikator/snmptn")
+    public String statistikVerifSNMPTN (Model model)
     {
-        List<JalurModel> j = svs.getPaths();
-        ArrayList<StatistikManagerModel> paths = new ArrayList<StatistikManagerModel>();
-        ArrayList<StatistikManagerModel> regPaths = new ArrayList<StatistikManagerModel>();
-        ArrayList<StatistikManagerSummaryModel> sumPaths = new ArrayList<StatistikManagerSummaryModel>();
+        List<FakultasModel> f = sms.getFaculties();
+        ArrayList<StatistikVerifikatorSummaryModel> sumSNMPTNVerified = new ArrayList<StatistikVerifikatorSummaryModel>();
         
-        for(int i=0; i < j.size(); i++)
+        for(int i=0; i < f.size(); i++)
         {
-                StatistikManagerModel jalur = svs.selectJalur(j.get(i).getJalur_id());
-                if(jalur != null) { paths.add(jalur); }
-                
-                StatistikManagerModel jalur2 = svs.selectRegistranJalur(j.get(i).getJalur_id());
-                if(jalur2 != null) { regPaths.add(jalur2); }
-                
-                StatistikManagerSummaryModel jalur3 = svs.summaryPath(j.get(i).getJalur_id());
-                if(jalur3 != null) { sumPaths.add(jalur3); }
+                StatistikVerifikatorSummaryModel faculty = svs.summaryFacultySNMPTN(f.get(i).getFakultas_id());
+                if(faculty != null) { sumSNMPTNVerified.add(faculty); }
         }
         
-        model.addAttribute("paths", paths);
-        model.addAttribute("regPaths", regPaths);
-        model.addAttribute("sumPaths", sumPaths);
+        model.addAttribute("sumSNMPTNVerified", sumSNMPTNVerified);
         
-        return "manager_pendidikan-statistik_verifikator_jalur";
+        
+        ArrayList<StatistikVerifikatorSummaryModel> sumPPKBVerified = new ArrayList<StatistikVerifikatorSummaryModel>();
+        
+        for(int i=0; i < f.size(); i++)
+        {
+                StatistikVerifikatorSummaryModel faculty = svs.summaryFacultyPPKB(f.get(i).getFakultas_id());
+                if(faculty != null) { sumPPKBVerified.add(faculty); }
+        }
+        
+        model.addAttribute("sumPPKBVerified", sumPPKBVerified);
+        
+        
+        ArrayList<StatistikVerifikatorSummaryModel> sumTalentVerified = new ArrayList<StatistikVerifikatorSummaryModel>();
+        
+        for(int i=0; i < f.size(); i++)
+        {
+                StatistikVerifikatorSummaryModel faculty = svs.summaryFacultyTalent(f.get(i).getFakultas_id());
+                if(faculty != null) { sumTalentVerified.add(faculty); }
+        }
+        
+        model.addAttribute("sumTalentVerified", sumTalentVerified);
+        return "manager_pendidikan-statistik_verifikator-snmptn";
+    }
+    
+    @RequestMapping("/manager-pendidikan/statistik-verifikator/sbmptn")
+    public String statistikVerifSBMPTN (Model model)
+    {
+        List<FakultasModel> f = sms.getFaculties();
+        
+        ArrayList<StatistikVerifikatorSummaryModel> sumSBMPTNVerified = new ArrayList<StatistikVerifikatorSummaryModel>();
+        
+        for(int i=0; i < f.size(); i++)
+        {
+                StatistikVerifikatorSummaryModel faculty = svs.summaryFacultySBMPTN(f.get(i).getFakultas_id());
+                System.out.println (faculty.getTotal ());
+                if(faculty != null) { sumSBMPTNVerified.add(faculty); }
+        }
+        
+        model.addAttribute("sumSBMPTNVerified", sumSBMPTNVerified);
+        
+        
+        ArrayList<StatistikVerifikatorSummaryModel> sumSBMPTNVerifiedReg = new ArrayList<StatistikVerifikatorSummaryModel>();
+        
+        for(int i=0; i < f.size(); i++)
+        {
+                StatistikVerifikatorSummaryModel faculty = svs.summaryFacultySBMPTNReg(f.get(i).getFakultas_id());
+                if(faculty != null) { sumSBMPTNVerifiedReg.add(faculty); }
+        }
+        
+        model.addAttribute("sumSBMPTNVerifiedReg", sumSBMPTNVerifiedReg);
+        
+        
+        ArrayList<StatistikVerifikatorSummaryModel> sumSBMPTNVerifiedPar = new ArrayList<StatistikVerifikatorSummaryModel>();
+        
+        for(int i=0; i < f.size(); i++)
+        {
+                StatistikVerifikatorSummaryModel faculty = svs.summaryFacultySBMPTNPar(f.get(i).getFakultas_id());
+                if(faculty != null) { sumSBMPTNVerifiedPar.add(faculty); }
+        }
+        
+        model.addAttribute("sumSBMPTNVerifiedPar", sumSBMPTNVerifiedPar);
+        return "manager_pendidikan-statistik_verifikator-sbmptn";
+    }
+    
+    @RequestMapping("/manager-pendidikan/statistik-verifikator/mandiri")
+    public String statistikVerifMANDIRI (Model model)
+    {
+        List<FakultasModel> f = sms.getFaculties();
+        
+        ArrayList<StatistikVerifikatorSummaryModel> sumMANDIRIVerified = new ArrayList<StatistikVerifikatorSummaryModel>();
+        
+        for(int i=0; i < f.size(); i++)
+        {
+                StatistikVerifikatorSummaryModel faculty = svs.summaryFacultyMANDIRI(f.get(i).getFakultas_id());
+                if(faculty != null) { sumMANDIRIVerified.add(faculty); }
+        }
+        
+        model.addAttribute("sumMANDIRIVerified", sumMANDIRIVerified);
+        
+        
+        ArrayList<StatistikVerifikatorSummaryModel> sumMANDIRIRegVerified = new ArrayList<StatistikVerifikatorSummaryModel>();
+        
+        for(int i=0; i < f.size(); i++)
+        {
+                StatistikVerifikatorSummaryModel faculty = svs.summaryFacultyMANDIRIReg(f.get(i).getFakultas_id());
+                if(faculty != null) { sumMANDIRIRegVerified.add(faculty); }
+        }
+        
+        model.addAttribute("sumMANDIRIRegVerified", sumMANDIRIRegVerified);
+        
+        
+        ArrayList<StatistikVerifikatorSummaryModel> sumMANDIRIParVerified = new ArrayList<StatistikVerifikatorSummaryModel>();
+        
+        for(int i=0; i < f.size(); i++)
+        {
+                StatistikVerifikatorSummaryModel faculty = svs.summaryFacultyMANDIRIPar(f.get(i).getFakultas_id());
+                if(faculty != null) { sumMANDIRIParVerified.add(faculty); }
+        }
+        
+        model.addAttribute("sumMANDIRIParVerified", sumMANDIRIParVerified);
+        
+        
+        ArrayList<StatistikVerifikatorSummaryModel> sumMANDIRIKKIVerified = new ArrayList<StatistikVerifikatorSummaryModel>();
+        
+        for(int i=0; i < f.size(); i++)
+        {
+                StatistikVerifikatorSummaryModel faculty = svs.summaryFacultyMANDIRIKKI(f.get(i).getFakultas_id());
+                if(faculty != null) { sumMANDIRIKKIVerified.add(faculty); }
+        }
+        
+        model.addAttribute("sumMANDIRIKKIVerified", sumMANDIRIKKIVerified);
+        return "manager_pendidikan-statistik_verifikator-mandiri";
     }
 }
