189b2480270d9aa69cffd4d16e560ef58f101b87
widiartoa <widiartoaww@gmail.com>
2017-05-20 08:27:54 +0700

    Deploy config

--- src/main/java/id/ac/univ/regismaba/RegismabaApplication.java
+++ src/main/java/id/ac/univ/regismaba/RegismabaApplication.java
@@ -3,7 +3,9 @@ package id.ac.univ.regismaba;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.Bean;
 
 import id.ac.univ.regismaba.storage.StorageProperties;
@@ -11,8 +13,13 @@ import id.ac.univ.regismaba.storage.StorageService;
 
 @EnableConfigurationProperties(StorageProperties.class)
 @SpringBootApplication
-public class RegismabaApplication {
+public class RegismabaApplication extends SpringBootServletInitializer {
 
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(RegismabaApplication.class);
+	}
+	
 	public static void main(String[] args) {
 		SpringApplication.run(RegismabaApplication.class, args);
 	}
